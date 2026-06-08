f510b469ced67ad6da948d6abc5cd47cb084e6ba
ApuKM <apukumar180@gmail.com>
2026-06-09 01:44:10 +0600

    Add: new job post to the database

---
+++ index.js
@@ -0,0 +1,70 @@
+// server.js
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+if (!uri) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env",
+  );
+}
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function startServer() {
+  try {
+    await client.connect();
+
+    const db = client.db(process.env.DB_NAME)
+    const jobsCollection = db.collection("jobs");
+
+    app.get("/api/jobs", async(req, res) => {
+        const {companyId, status} = req.query;
+        let query ={}
+        if(companyId){
+            query.companyId = companyId
+        }
+        if(status){
+            query.status = status
+        }
+        const result = await jobsCollection.find(query).toArray();
+        res.send(result)
+    })
+
+    app.post("/api/jobs", async(req, res) => {
+        const job = req.body;
+        const result = await jobsCollection.insertOne(job);
+        res.send(result)
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("🍃 Successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("❌ Database connection failed:", error);
+    // process.exit(1);
+  }
+}
+
+startServer();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Server is running on port ${port}`);
+});
